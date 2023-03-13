8034f64e714c1400da584d03607ac159841c05df
RyoheiKomori <59478903+RyoheiKomori@users.noreply.github.com>
2023-03-13 20:34:25 +0900

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./styles.css";
 import { Button, ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
 
 export default function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button>ボタン</Button>
+      <p>aaaaaa</p>
     </ChakraProvider>
   );
 }
