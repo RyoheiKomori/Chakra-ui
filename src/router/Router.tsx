fdce7686c7b63b3264e628c07fa337680d20e080
RyoheiKomori <59478903+RyoheiKomori@users.noreply.github.com>
2023-03-16 22:36:11 +0900

    routing1

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,17 @@
 import { FC, memo } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Login } from "../components/pages/Login";
+import { homeRoutes } from "../router/HomeRoutes";
 
 export const Router: FC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
+      <Route>
+        {homeRoutes.map((route) => (
+          <Route path={route.path} element={route.children}></Route>
+        ))}
+      </Route>
     </Routes>
   );
 });
