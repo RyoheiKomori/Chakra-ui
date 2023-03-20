2a0165bfb7dd7147669b3f587e28ebf31c08ff7d
RyoheiKomori <59478903+RyoheiKomori@users.noreply.github.com>
2023-03-20 21:36:10 +0900

    add Layout

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -1,6 +1,7 @@
 import { UserManagement } from "../components/pages/UserManagement";
 import { Setting } from "../components/pages/Setting";
 import { Home } from "../components/pages/Home";
+import { Page404 } from "../components/pages/Page404";
 
 export const homeRoutes = [
   {
@@ -14,5 +15,9 @@ export const homeRoutes = [
   {
     path: "home/setting",
     children: <Setting />
+  },
+  {
+    path: "*",
+    children: <Page404 />
   }
 ];
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,16 +2,19 @@ import { FC, memo } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Login } from "../components/pages/Login";
 import { homeRoutes } from "../router/HomeRoutes";
+import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const Router: FC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
-      <Route>
-        {homeRoutes.map((route) => (
-          <Route path={route.path} element={route.children}></Route>
-        ))}
-      </Route>
+      {homeRoutes.map((route) => (
+        <Route
+          path={route.path}
+          element={<HeaderLayout>{route.children}</HeaderLayout>}
+        ></Route>
+      ))}
     </Routes>
   );
 });
