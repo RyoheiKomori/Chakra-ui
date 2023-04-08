f224a2fae89c6a187b792bf5a4aad2947526a2d1
RyoheiKomori <59478903+RyoheiKomori@users.noreply.github.com>
2023-04-08 14:35:53 +0900

    HeaderRouring

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,11 +1,24 @@
+//下記でuseCallbackの依存配列の非活性化を行う
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
-import { FC, memo } from "react";
+import { FC, memo, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
 
 export const Header: FC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const Navigate = useNavigate();
+
+  const onClickHome = useCallback(() => Navigate("/home"), []);
+  const onClickUserManagement = useCallback(
+    () => Navigate("/home/user_management"),
+    []
+  );
+  const onClickSetting = useCallback(() => Navigate("/home/setting"), []);
+
   return (
     <>
       <Flex
@@ -16,7 +29,13 @@ export const Header: FC = memo(() => {
         justify="space-between"
         padding={{ base: 3, md: 5 }}
       >
-        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Flex
+          align="center"
+          as="a"
+          mr={8}
+          _hover={{ cursor: "pointer" }}
+          onClick={onClickHome}
+        >
           <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
             ユーザー管理アプリ
           </Heading>
@@ -28,14 +47,20 @@ export const Header: FC = memo(() => {
           display={{ base: "none", md: "flex" }}
         >
           <Box pr={4}>
-            <Link>ユーザー一覧</Link>
+            <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
           </Box>
 
-          <Link>設定</Link>
+          <Link onClick={onClickSetting}>設定</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <MenuDrawer onClose={onClose} isOpen={isOpen} />
+      <MenuDrawer
+        onClose={onClose}
+        isOpen={isOpen}
+        onClickHome={onClickHome}
+        onClickUserManagement={onClickUserManagement}
+        onClickSetting={onClickSetting}
+      />
     </>
   );
 });
