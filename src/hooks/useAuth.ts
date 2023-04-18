c009aa12684339a9051545fbcc410c67f972c55c
RyoheiKomori <59478903+RyoheiKomori@users.noreply.github.com>
2023-04-18 15:05:00 +0900

    add login messages

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,11 @@ import { useCallback, useState } from "react";
 import axios from "axios";
 import { User } from "../types/api/user";
 import { useNavigate } from "react-router-dom";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const native = useNavigate();
+  const { showMessage } = useMessage();
 
   const [loading, setLoading] = useState(false);
 
@@ -16,15 +18,18 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
             native("/home");
           } else {
-            alert("ユーザーが見つかりません");
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
           }
         })
-        .catch(() => alert("ログインできません"))
+        .catch(() =>
+          showMessage({ title: "ログインできません", status: "error" })
+        )
         .finally(() => setLoading(false));
     },
-    [native]
+    [native, showMessage]
   );
   return { login, loading };
 };
